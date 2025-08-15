23bd1ad17d32dac9b63855b8e43b4e7e02928e63
himonshuuu <saikiahimangshu1256@gmail.com>
2025-08-15 05:41:22 +0530

    chore: init

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 	import '../app.css';
-	import favicon from '$lib/assets/favicon.svg';
+	import favicon from '$lib/assets/favicon.png';
 	import Navbar from '$components/layout/navbar.svelte';
 	import Footer from '$components/layout/footer.svelte';
 	let { children } = $props();
