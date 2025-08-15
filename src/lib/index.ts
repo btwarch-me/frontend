f4c5512d298c30b9dd5a15fa6c2eb688c54b74f4
himonshuuu <saikiahimangshu1256@gmail.com>
2025-08-15 05:43:09 +0530

    chore: init

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,5 +1,5 @@
 // place files you want to import through the `$lib` alias in this folder.
-import archimage from '$lib/assets/arch.png';
+
 import fuckwin from '$lib/assets/fuck.png';
 
-export { archimage, fuckwin };
+export { fuckwin };
